848640ac04621a05783f6042945f506b4b75d197
Станислав Земляков <redtom@yandex.ru>
2019-04-19 20:15:36 +0300

    Initial commit

---
+++ src/main/kotlin/com/github/rcd27/cnv/CNVApplication.kt
@@ -0,0 +1,5 @@
+package com.github.rcd27.cnv
+
+fun main() {
+
+}
---
+++ src/test/kotlin/com/github/rcd27/cnv/MainTestCase.kt
@@ -0,0 +1,11 @@
+package com.github.rcd27.cnv
+
+import org.junit.Test
+
+class MainTestCase {
+
+    @Test
+    fun main() {
+
+    }
+}
