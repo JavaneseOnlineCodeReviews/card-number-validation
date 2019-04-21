6acc1de12148360da36708e8a83e21f017478713
Станислав Земляков <redtom@yandex.ru>
2019-04-21 14:14:43 +0300

    Get response from binlist API

--- src/main/kotlin/com/github/rcd27/cnv/CardInfoService.kt
+++ src/main/kotlin/com/github/rcd27/cnv/CardInfoService.kt
@@ -1,5 +1,11 @@
 package com.github.rcd27.cnv
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+
 interface CardInfoService {
 
     fun getCardInfo(cardNumber: String): CreditCard
@@ -15,4 +21,29 @@ interface CardInfoService {
             return CreditCard("x", "y")
         }
     }
+}
+
+fun main() {
+    val url = URL("https://lookup.binlist.net/45717360")
+    val connection = (url.openConnection() as HttpURLConnection).apply {
+        requestMethod = "GET"
+        addRequestProperty("Accept-Version", "3")
+    }
+
+    println("Sending 'GET' request...")
+    println("Response code: ${connection.responseCode}")
+    println("Response message: ${connection.responseMessage}")
+
+    val `in` = BufferedReader(InputStreamReader(connection.inputStream))
+    var inputLine = `in`.readLine()
+    val response = StringBuffer()
+
+    while (inputLine != null) {
+        response.append(inputLine)
+        inputLine = `in`.readLine()
+    }
+    `in`.close()
+
+    println("Response: $response")
+
 }
